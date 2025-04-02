dbb3e2e6bf8b974f841b2ef2371605d7364878c6
Patrick Pramberger <s54469@edu.campus02.at>
2025-04-02 09:20:27 +0200

    finished MainClass

--- src/main/java/at/campus02/bsd/MainClass.java
+++ src/main/java/at/campus02/bsd/MainClass.java
@@ -10,6 +10,8 @@ public class MainClass {
         System.out.println("Result subtract: " + Calculator.subtract(number1, number2));
         System.out.println("Result multiply: " + Calculator.multiply(number1, number2));
         System.out.println("Result division: " + Calculator.divide(number1, number2));
+        System.out.println();
+        System.out.println("Pramberger Patrick");
 
         System.out.println("Calculation Finished");
     }
