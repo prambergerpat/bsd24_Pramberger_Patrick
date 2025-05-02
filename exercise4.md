8176f53c36475c3b49e670a1ef2c31c5a75455d0
Patrick Pramberger <s54469@edu.campus02.at>
2025-05-02 18:46:04 +0200

    creating a conflict

---
+++ exercise4.md
@@ -0,0 +1,5 @@
+# This is only to create a conflict
+
+- Conflict one
+- Conflict two
+- Conflict three
---
+++ src/main/java/at/campus02/bsd/JarClass.java
@@ -0,0 +1,9 @@
+package at.campus02.bsd;
+
+public class JarClass {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Graz!");
+        System.out.println(args[0]);
+    }
+}
